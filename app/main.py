8b4b3ca1c677a81bf4359f8fc41e3f4d39895f6c
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2022-10-14 06:49:21 +0000

    Added .env.example file

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
-from fastapi import FastAPI
+from typing import Dict
+from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import api
+from http import HTTPStatus
 
-app = FastAPI()
+# Define application
+app = FastAPI(
+    title="FastAPI Blog App",
+    description="Create Blogs.",
+    version="0.1",
+)
 
 app.include_router(api.router)
 
@@ -17,7 +24,18 @@ app.add_middleware(
 )
 
 
-@app.get("/", tags=["home"])
+@app.get("/home", tags=["home"])
 def get_home():
     """Root Endpoint"""
     return {"message": "Hello World"}
+
+
+@app.get("/")
+def _index() -> Dict:
+    """Health check."""
+    response = {
+        "message": HTTPStatus.OK.phrase,
+        "status-code": HTTPStatus.OK,
+        "data": {},
+    }
+    return response
